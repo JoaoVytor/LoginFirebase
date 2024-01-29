ba3275a76792ef526416d7da4bf96b452cde84e6
JoaoVytor <jjoaovitor.contato@gmail.com>
2024-01-29 18:14:32 -0300

    Create screen login, labels and button for login/register.

---
+++ LoginFirebase/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  LoginFirebase
+//
+//  Created by João Vitor Sousa on 29/01/24.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login Firebase"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = UIColor.blue
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let cadastrarButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Cadastrar", for: .normal)
+        button.backgroundColor = UIColor.gray
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(cadastrarButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(titleLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(cadastrarButton)
+
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80),
+            
+            emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emailTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            emailTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginButton.heightAnchor.constraint(equalToConstant: 40),
+
+            cadastrarButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 20),
+            cadastrarButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cadastrarButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            cadastrarButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            cadastrarButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+
+    @objc func loginButtonTapped() {
+        print("Login")
+    }
+
+    @objc func cadastrarButtonTapped() {
+        print("Cadastrar")
+    }
+}
+
